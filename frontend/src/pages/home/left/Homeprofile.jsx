b8257a48af41bba0519962433cc59a7b3b760214
MAAZ9876 <amaaz987654321@gmail.com>
2026-08-30 09:24:56 +0530

    Update Homeprofile.jsx

--- frontend/src/pages/home/left/Homeprofile.jsx
+++ frontend/src/pages/home/left/Homeprofile.jsx
@@ -8,7 +8,7 @@ import {useSelector} from 'react-redux';
 import { Chat,AccountCircle, ExitToApp,Settings,Home} from "@material-ui/icons";
 const Homeprofile = () => {
 
-    const pf="https://notesharingbackend-ankitkr437.onrender.com/images/";
+    const pf="http://localhost:8000/images/";
   
     const {currentUser,isFetching} = useSelector((state)=>state.user)
     const user=currentUser
